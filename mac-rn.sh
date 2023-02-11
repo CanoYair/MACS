b244b6fb0fc5e45083365803f41f27b36edc3ab0
Cano23& <104598388+CanoYair@users.noreply.github.com>
2023-02-11 13:11:15 -0600

    Add files via upload

---
+++ mac-rn.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+brew install rbenv
+
+
+
+rbenv install 2.7.5
+
+
+
+
+rbenv global 2.7.5
+rbenv rehash
+
+
+
+
+gem install bundler
+
+
+
+
+
+
+brew install node
+
+
+brew install watchman
+
+
+
+
+
+
+npm install -g react-native-cli
+
+react-native-cli: 2.0.1
+react-native: n/a - not inside a React Native project
+
+
+
+sudo gem install cocoapods
+
+
+brew tap AdoptOpenJDK/openjdk
+brew cask install adoptopenjdk8
+
+
+openjdk version "1.8.0_222"
+OpenJDK Runtime Environment (AdoptOpenJDK)(build 1.8.0_222-b10)
+OpenJDK 64-Bit Server VM (AdoptOpenJDK)(build 25.222-b10, mixed mode)
+
+
+
+
+
+npx react-native init SampleApp
+
+
+cd SampleApp
+
+
+npm run ios
+
+
+cd SampleApp
+
+
+npm run android
